9000be6750fc2e3abdb5d5051ca35763fd86a117
Carlos Takeshi Sato <carlostakeshi100@gmail.com>
2024-03-31 12:59:30 -0300

    refactor: question is passed in query parameters on interaction endpoint

--- src/api/v1/endpoints/interaction.py
+++ src/api/v1/endpoints/interaction.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter, status, HTTPException
 from crud.interaction_spec import InteractionSpec
-from schemas.interaction_schema import InteractionSchema
 from models.interaction_model import InteractionModel
 
 
 router = APIRouter()
 
 # Get
-@router.post("/{char_name}", status_code=status.HTTP_200_OK, response_model=InteractionModel)
-async def get_character_response(char_name: str, payload: InteractionSchema):
+@router.get("/{char_name}", status_code=status.HTTP_200_OK, response_model=InteractionModel)
+async def get_character_response(char_name: str, question: str):
     interaction = InteractionSpec()
-    formatted_prompt = interaction.format_prompt(char_name=char_name, question=payload.question)
+    formatted_prompt = interaction.format_prompt(char_name=char_name, question=question)
 
     if formatted_prompt:
         response = interaction.ask_gemini(formatted_prompt)
